5f4e12dde19f338aa661d516084cbe1366031a3a
meet3011 <47962133+meet3011@users.noreply.github.com>
2021-05-07 19:50:22 +0530

    Update DetailsScreen.js

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View,Text,Button,StyleSheet} from 'react-native';
+import {View,Text,StyleSheet} from 'react-native';
+import { FAB } from 'react-native-paper';
 
 const DetailsScreen = ({navigation}) =>{
     return(
             <View style={{flex:1, alignItems:'center',justifyContent:'center'}}>
               <Text>HomeScreen</Text>
+              <FAB
+                style={styles.fab}
+                small
+                icon="plus"
+                onPress={() => navigation.navigate('Map')}
+              />
             </View>
     );
   };
@@ -15,5 +22,11 @@ const styles= StyleSheet.create({
         flex:1,
         alignItems:'center',
         justifyContent:'center'
+    },
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      right: 0,
+      bottom: 0,
     },
 });
