93fc4d6f699a735569cce5241e99ba760013366a
meet3011 <47962133+meet3011@users.noreply.github.com>
2021-05-15 23:14:28 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -64,7 +64,7 @@ export default function HomeScreen({ navigation }) {
                 style={styles.fab}
                 small
                 icon="plus"
-                onPress={() => navigation.navigate('Map')}
+                onPress={() => navigation.navigate('ReportScreen')}
             />
            
         </View>
